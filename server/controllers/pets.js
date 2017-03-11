fc662fb1165daafe506327af8125d1fda3cccad6
Abhishek Wadhwa <doody_since1990@yahoo.co.in>
2017-03-11 17:18:39 -0600

    adding end point for lookup by id and lookup all

--- server/controllers/pets.js
+++ server/controllers/pets.js
@@ -1,18 +1,39 @@
-"use strict"
-const Pet = require('../models').Pet;
+    "use strict"
+    const Pet = require('../models').Pet;
 
-module.exports = {
-  create(req, res) {
-    return Pet
-      .create({
-        name: req.body.name,
-        type: req.body.type,
-        breed: req.body.breed,
-        location: req.body.location,
-        latitude: req.body.latitude,
-        longitude: req.body.longitude
-      })
-      .then(pet => res.status(201).send(pet))
-      .catch(error => res.status(400).send(error));
-  },
-};
+    module.exports = {
+      create(req, res) {
+        return Pet
+          .create({
+            name: req.body.name,
+            type: req.body.type,
+            breed: req.body.breed,
+            location: req.body.location,
+            latitude: req.body.latitude,
+            longitude: req.body.longitude
+          })
+          .then(pet => res.status(201).send(pet))
+          .catch(error => res.status(400).send(error));
+      },
+
+      list(req, res) {
+       return Pet
+          .all()
+          .then(pet => res.status(200).send(pet))
+          .catch(error => res.status(400).send(error));
+      },
+
+      retrieve(req, res) {
+       return Pet
+          .findById(req.params.petId)
+          .then(pet => {
+            if (!pet) {
+              return res.status(404).send({
+               message: 'Pet Not Found',
+            });
+          }
+          return res.status(200).send(pet);
+        })
+        .catch(error => res.status(400).send(error));
+    },
+    };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 const petsController = require('../controllers').pets;
 
 module.exports = (app) => {
+  app.get('/api/pets/:petId', petsController.retrieve);
   app.post('/api/pets', petsController.create);
+  app.get('/api/pets', petsController.list);
 };
